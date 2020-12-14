a410b0d34f9ee815c7381b521c6ec5e4d93ccf76
nngocjade <nngocjade@gmail.com>
2020-12-15 05:57:19 +0700

    adding files

---
+++ Assignment1.js
@@ -0,0 +1,20 @@
+// - Create a JavaScript file for each assignment
+// - Put the question as a comment on top of your solution
+// - Organize your code so that it's easy to read and review
+
+// Assignment 1 - Area of a circle
+// Log the string "Area of a circle" to the console.
+// Declare a variable called pi and assign the value 3.14 to it in one statement.
+// Log the type of pi to the console.
+// Declare a variable called radius and assign the value 5 to it in one statement.
+// Log the type of radius to the console.
+// Declare a variable called area which is calculated by pi and radius to get the area of a circle (the area of a circle is pi times the radius squared).
+
+let pi = 3.14;
+console.log(pi);
+
+let radius = 5;
+console.log(radius);
+
+let area = pi * radius * radius;
+console.log(area);
