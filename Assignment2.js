84006b2cbbfa1c73e61327de2b7cb91a38052ef4
nngocjade <nngocjade@gmail.com>
2020-12-15 07:07:07 +0700

    backing up

---
+++ Assignment2.js
@@ -0,0 +1,50 @@
+// Assignment 2 - Operators & Comparison
+// Print the results of the expressions below in this format:
+
+// EXPRESSION = RESULT
+// Example:
+
+// "Coder" + "School" = "CoderSchool"
+// Before you execute the code, predict in your head what will be returned.
+
+console.log("-----------------------------------------");
+
+let assignment2 = "assignment2" + " : " + "Operators" + " +  & " + "Comparison";
+console.log(assignment2);
+
+console.log("Operators");
+
+let ten = 10;
+let twentyFour = 24;
+let addition = 10 + 24;
+console.log(ten + " + " + twentyFour + " = " + addition);
+//
+let string = "10" + "24";
+console.log('"10"' + " + " + '"24"' + " = " + string);
+//
+let concatenation = "Hello" + " " + 2021;
+console.log('"Hello"' + " + " + '" " ' + " + " + 2021 + " = " + concatenation);
+
+// 1 + 2 * 3(1 + 3) ** 2;
+// 1 / 0;
+// 6 % 2;
+// 5.5 % 2;
+// Number("123");
+// typeof (1 + "");
+// Logical & Comparison;
+
+// 5 == "5";
+// 5 === "5";
+// 8 != 8.0;
+// 8 !== 8.0;
+// "true" === true;
+// 4 <= 4.0;
+// 7 >= 7;
+// true && true;
+// true && false;
+// true || true;
+// false || true;
+// !true;
+// !false;
+// false && (true || true);
+// (false && true) || true;
